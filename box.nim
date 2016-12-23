1276ad9993436687dffffa4c9433169edadc9a45
Enrique <niofis@gmail.com>
2016-12-23 00:18:11 +0000

    box.nim

---
+++ box.nim
@@ -0,0 +1,125 @@
+import
+    random,
+    sdl2/sdl,
+    sdl2/sdl_gfx_primitives as gfx,
+    math,
+    sequtils
+
+
+const
+  Title = "SDL2 Test"
+  ScreenW = 480 # Window width
+  ScreenH = 272 # Window height
+  WindowFlags = 0
+  RendererFlags = sdl.RendererAccelerated or sdl.RendererPresentVsync
+  OriginX = ScreenW / 2
+  OriginY = ScreenH / 2
+
+
+type
+  App = ref AppObj
+  AppObj = object
+    window*: sdl.Window # Window pointer
+    renderer*: sdl.Renderer # Rendering state pointer
+
+##########
+# COMMON #
+##########
+
+# Initialization sequence
+proc init(app: App): bool =
+  # Init SDL
+  if sdl.init(sdl.InitVideo) != 0:
+    sdl.logCritical(sdl.LogCategoryError,
+                    "Can't initialize SDL: %s",
+                    sdl.getError())
+    return false
+
+  # Create window
+  app.window = sdl.createWindow(
+    Title,
+    sdl.WindowPosUndefined,
+    sdl.WindowPosUndefined,
+    ScreenW,
+    ScreenH,
+    WindowFlags)
+  if app.window == nil:
+    sdl.logCritical(sdl.LogCategoryError,
+                    "Can't create window: %s",
+                    sdl.getError())
+    return false
+
+  # Create renderer
+  app.renderer = sdl.createRenderer(app.window, -1, RendererFlags)
+  if app.renderer == nil:
+    sdl.logCritical(sdl.LogCategoryError,
+                    "Can't create renderer: %s",
+                    sdl.getError())
+    return false
+
+  #sdl.logInfo(sdl.LogCategoryApplication, "SDL initialized successfully")
+  return true
+
+
+# Shutdown sequence
+proc exit(app: App) =
+  app.renderer.destroyRenderer()
+  app.window.destroyWindow()
+  #sdl.logInfo(sdl.LogCategoryApplication, "SDL shutdown completed")
+  sdl.quit()
+
+
+# Event handling
+# Return true on app shutdown request, otherwise return false
+proc events(pressed: var seq[sdl.Keycode]): bool =
+  result = false
+  var e: sdl.Event
+  if pressed != nil:
+    pressed = @[]
+
+  while sdl.pollEvent(addr(e)) != 0:
+
+    # Quit requested
+    if e.kind == sdl.Quit:
+      return true
+
+    # Key pressed
+    elif e.kind == sdl.KeyDown:
+      # Add pressed key to sequence
+      if pressed != nil:
+        pressed.add(e.key.keysym.sym)
+
+      # Exit on Escape key press
+      if e.key.keysym.sym == sdl.K_Escape:
+        return true
+
+
+########
+# MAIN #
+########
+
+var
+  app = App(window: nil, renderer: nil)
+  done = false # Main loop exit condition
+  pressed: seq[sdl.Keycode] = @[] # Pressed keys
+
+if init(app):
+
+  # Main loop
+  while not done:
+    # Clear screen with draw color
+    discard app.renderer.setRenderDrawColor(0x00, 0x00, 0x00, 0xFF)
+    if app.renderer.renderClear() != 0:
+      sdl.logWarn(sdl.LogCategoryVideo,
+                  "Can't clear screen: %s",
+                  sdl.getError())
+
+    # Update renderer
+    app.renderer.renderPresent()
+
+    # Event handling
+    done = events(pressed)
+
+# Shutdown
+exit(app)
+
