700948b5e15e9cf3d74c28a703a862dc64506011
Enrique <niofis@gmail.com>
2016-12-23 01:44:31 +0000

    looking to update pixels

--- box.nim
+++ box.nim
@@ -12,8 +12,6 @@ const
   ScreenH = 272 # Window height
   WindowFlags = 0
   RendererFlags = sdl.RendererAccelerated or sdl.RendererPresentVsync
-  OriginX = ScreenW / 2
-  OriginY = ScreenH / 2
 
 
 type
@@ -102,17 +100,33 @@ var
   app = App(window: nil, renderer: nil)
   done = false # Main loop exit condition
   pressed: seq[sdl.Keycode] = @[] # Pressed keys
+  texture = app.renderer.createTexture(sdl.PIXEL_FORMAT_ARGB8888, sdl.TEXTUREACCESS_STREAMING, 100, 100)
+  pixels: array[100*100, uint32]
 
 if init(app):
 
   # Main loop
   while not done:
     # Clear screen with draw color
-    discard app.renderer.setRenderDrawColor(0x00, 0x00, 0x00, 0xFF)
-    if app.renderer.renderClear() != 0:
-      sdl.logWarn(sdl.LogCategoryVideo,
-                  "Can't clear screen: %s",
-                  sdl.getError())
+    #discard app.renderer.setRenderDrawColor(0x00, 0x00, 0x00, 0xFF)
+    #if app.renderer.renderClear() != 0:
+    #  sdl.logWarn(sdl.LogCategoryVideo,
+    #              "Can't clear screen: %s",
+    #              sdl.getError())
+
+    #pixels[100*100 - 1] = cast[int32](0xFFFFFFFF)
+    for x in 0..(100*100-1):
+      pixels[x] = cast[uint32](0xAAAAAAAA)
+
+    if texture.updateTexture(nil, addr(pixels), 100*sizeof(uint32)) != 0:
+      sdl.logCritical(sdl.LogCategoryError,
+                    "Can't create renderer: %s",
+                    sdl.getError())
+
+
+    #texture.render(app.renderer, 0, 0)
+
+    discard app.renderer.renderCopy(texture, nil, nil)
 
     # Update renderer
     app.renderer.renderPresent()
